7bd55eab93cb9e44957ef6dc7277f6426e889b62
JvZhao <122228569+xxsars@users.noreply.github.com>
2026-04-07 02:21:45 +0800

    Update dsu.cpp

--- Template/数据结构/并查集/dsu.cpp
+++ Template/数据结构/并查集/dsu.cpp
@@ -1,25 +1,19 @@
-struct dsu {
-    vector<int> f, siz;
-    dsu(int n) {
-        f.resize(n); iota(f.begin(), f.end(), 0);
-        siz.assign(n, 1);
-    }
-    int find(int x) {
-        while (x != f[x]) x = f[x] = f[f[x]];
-        return x;
-    }
-    bool same(int x, int y) {
-        return find(x) == find(y);
-    }
-    bool merge(int x, int y) {
-        x = find(x), y = find(y);
-        if (x == y) return false;
-        if (siz[x] <= siz[y]) swap(x, y);
-        siz[x] += siz[y];
-        f[y] = x;
-        return true;
-    }
-    int size(int x) {
-        return siz[find(x)];
-    }
+struct Dsu { // 如果iddex = 1, cc初始化为n - 1
+  vector<int> f, siz; int cc;
+  Dsu(int n): f(n), siz(n, 1), cc(n) { iota(f.begin(), f.end(), 0); }
+  int find(int x) {
+    while (x != f[x]) x = f[x] = f[f[x]];
+    return x;
+  }
+  bool same(int x, int y) { return find(x) == find(y); }
+  int size(int x) { return siz[find(x)]; }
+  bool merge(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    if (siz[x] > siz[y]) swap(x, y);
+    siz[y] += siz[x];
+    f[x] = y;
+    cc -= 1;
+    return true;
+  }
 };
